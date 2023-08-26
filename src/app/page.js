b7fb58d72defc0aba108bdff195d7a1fe06f20f4
Abu-Jayed <aj.webdevlopment@gmail.com>
2023-08-26 17:09:12 +0600

    statistic section done

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Hero from './components/Hero'
+import Stats from './components/Stats'
 
 export default function Home() {
   return (
     <>
       <Hero></Hero>
+      <Stats></Stats>
     </>
   )
 }
